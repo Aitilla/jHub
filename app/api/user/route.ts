47235273074b954f565ae51c0b09bd02772acec0
Joachim Hertaas <joachher@ulrik.uio.no>
2025-05-20 12:54:13 +0200

    supabase fetch

---
+++ app/api/user/route.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+
+export async function GET() {
+  let { data: User, error } = await supabase.from("User").select("*");
+  console.log(User)
+}
